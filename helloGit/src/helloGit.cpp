408fa18178a9e7a4fb980afe0e01c79b09f3a15b
subrota <subrota_002@yahoo.com>
2017-02-11 19:58:48 +0800

    first commit

---
+++ helloGit/src/helloGit.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : helloGit.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Dhaka, Bangladesh" << endl;
+	return 0;
+}
